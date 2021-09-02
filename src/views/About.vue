22dc7a284fa937f4e63393e623e7e3d231262e51
kangdan <jayadanielsutra@gmail.com>
2021-09-02 16:00:18 +0700

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,57 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input type="file" accept="image/*" @change="getFile">
+    <div>
+        <img id="input-preview" alt="This is the preview of the image you are going to upload."/>
+    </div>
+    <div>
+        <img id="output-preview" alt="This is the compressed result of the image you will upload."/>  
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      img: null,
+      in: null,
+      out: null,
+      file: null,
+    }
+  },
+  methods: {
+    getFile(event) {
+      const file = event.target.files[0]
+      this.file = file
+      this.runFn(file)
+    },
+    doDrawing(file) {
+      return new Promise((resolve) => {
+        const canvas = document.createElement("canvas")
+        canvas.width = 300
+        canvas.height = 300
+        
+        const inputPreview = document.getElementById("input-preview");
+
+        inputPreview.src = URL.createObjectURL(file)
+        const ctx = canvas.getContext("2d")
+
+        ctx.drawImage(inputPreview, 0, 0, canvas.width, canvas.height)
+
+        ctx.canvas.toBlob((blob) => {
+            resolve(blob);
+          }, "image/png"); 
+        })
+    },
+    async runFn(file) {
+      const blog = await this.doDrawing(file)
+      console.log(blog)
+    }
+  },
+  created() {
+    
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <input type="file" @change="onFileClick">
+    <button @click="uploadImg">Upload</button>
   </div>
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity';
+import { watch } from '@vue/runtime-core';
+import {computed} from 'vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  setup() {
+
+   const img = reactive({
+      original: null,
+      inputPreview: null,
+      originalHeight: null,
+      originalWidth: null,
+      outPreview: null,
+      test:null
+   }) 
+
+   function compressImage(image, scale, initalWidth, initalHeight){
+    return new Promise((resolve) => {
+        const canvas = document.createElement("canvas");
+
+        canvas.width = scale * initalWidth;
+        canvas.height = scale * initalHeight;
+
+        const ctx = canvas.getContext("2d");
+
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+        
+        ctx.canvas.toBlob((blob) => {
+            resolve(blob);
+        }, "image/png"); 
+    }); 
+    }
+
+
+    const getHeightAndWidthFromDataUrl = dataURL => new Promise(resolve => {
+      const img = new Image()
+      img.onload = () => {
+        resolve({
+          height: img.height,
+          width: img.width
+        })
+      }
+      img.src = dataURL
+    })
+
+    async function onFileClick(event) {
+
+      img.original = event.target.files[0]
+      
+      if(img.original === null) {
+        return
+      }
+      var imagePre = new Image()
+      imagePre.src = URL.createObjectURL(img.original)
+      img.inputPreview = imagePre
+
+      getHeightAndWidthFromDataUrl(imagePre.src).then(dimensions => {
+        img.originalWidth = dimensions.width
+        img.originalHeight = dimensions.height
+      })
+
+    }
+
+    const orgWidth = computed(() => img.originalWidth)
+    const orgHeight = computed(() => img.originalHeight)
+    
+
+    watch([orgHeight, orgWidth], async function() {
+        
+        const MAX_WIDTH = 1000; //if we resize by width, this is the max width of compressed image
+        const MAX_HEIGHT = 1000; //if we resize by height, this is the max height of the compressed image
+      
+        const widthRatioBlob = await compressImage(img.inputPreview, MAX_WIDTH / orgWidth.value, orgWidth.value, orgHeight.value); 
+        // console.log(widthRatioBlob)
+        const heightRatioBlob = await compressImage(img.inputPreview, MAX_HEIGHT / orgHeight.value,  orgWidth.value, orgHeight.value);
+
+        const compressedBlob = widthRatioBlob.size > heightRatioBlob.size ? heightRatioBlob : widthRatioBlob;
+
+        var imageOut = new Image()
+        imageOut.src = URL.createObjectURL(compressedBlob)
+        img.outPreview = imageOut
+
+        URL.revokeObjectURL()
+    })
+
+
+    return {
+      onFileClick,
+      img
+    }
+    
   }
 }
 </script>
